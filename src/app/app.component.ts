b164910717ebd23a15153dcc7d520aa4d09d9e5d
Methinee Phormsorn <dagontunk1234@gmail.com>
2023-08-06 14:41:09 +0700

    insert store component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app',
   template:`
-    <div class="bg-success p-2 text-center text-white ">
-      This is Lazadee application !!! 
-    </div>`,
+    <store></store>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
