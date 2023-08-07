1f6ad6a40744c83f5e27a8ac90b5cccf69e3cc2a
Methinee Phormsorn <dagontunk1234@gmail.com>
2023-08-07 11:42:25 +0700

    Added routing config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template:`
-    <store></store>
+  template: `
+    <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css']
 })
